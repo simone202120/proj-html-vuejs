11f7161f0d023c2cc72c7d84a8a8859274856d14
Simone Fortunato <frtsmn94@gmail.com>
2022-09-05 15:20:53 +0200

    Jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyJumbotron />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyJumbotron from './components/MyJumbotron.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyJumbotron
+  },
+
+  data(){
+    return{
+
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './style/general.scss';
+  @import './style/var.scss';
+  @import '~@fortawesome/fontawesome-free/css/all.css'
+
 </style>
---
+++ src/components/MyJumbotron.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="background ">
+        <div class="container flex spaceBetween">
+            <div class="logo">
+                <img id="logo" src="../assets/logotype.png" alt="">
+            </div>
+            
+            <div class="nav">
+                <ul class="flex">
+                    <li v-for="(voice, index) in MenuVoices" :key="index">
+                        <a href="">{{voice}}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="title">
+            <div class="flex">
+                <div class="barra"></div>
+                    <p>ALWAIS THE BEST WAY YOU NEED IT</p>
+                </div>
+                <H1>THE BEST BUSINESS CONSULTING</H1>
+                <div class="buttons flex">
+                    <button><a href="#"></a>READ MORE</button>
+                <div class="play flex">
+                    <a href=""><i class="fa-solid fa-play"></i></a>
+                </div>
+            </div>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'MyHeader',
+
+    data(){
+        return{
+            MenuVoices:['HOME', 'ABOUT', 'PROJECTS', 'SERVICES', 'BLOG','CONTACT']
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+     @import '../style/var.scss';
+   .background{
+    background-image: url('../assets/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg');
+    height: 100vh;
+    background-size: 60%;
+    background-repeat: no-repeat;
+    background-position-x: right;
+   
+   }
+   .spaceBetween{
+    justify-content: space-between;
+   }
+
+   li{
+    color: white;
+    padding: 20px 5px;
+   }
+   #logo{
+    height: 20px;
+   }
+   .logo{
+    padding: 20px;
+    height: 40px;
+   }
+   .nav{
+    height: 30px;
+   }
+   .title{
+    color: white;
+    width: 40%;
+    margin-left: 11%;
+    margin-top: 100px;
+   }
+   .barra{
+    width: 30px;
+    border-bottom:  2px solid $Mygreen;
+    margin-right: 15px;
+    margin-bottom: 3px;
+   }
+   button{
+    background-color: $Mygreen;
+    width: 150px;
+   }
+   .play{
+    width: 40px;
+    height: 40px;
+    border: 2px solid white;
+    border-radius: 50%;
+    margin-left: 20px;
+    justify-content: center;
+    align-items: center;
+
+    a{
+        color: $Mygreen;
+    }
+    
+   }
+   h1{
+    font-size: 95px;
+    margin:20px 0 40px 0;
+    line-height: 0.8;
+   }
+   
+</style>
